53000649f44f13309677401406fcf892661d1966
Rodrigo Prata <rodrigo-p-braga@telecom.com>
2022-06-01 20:10:18 -0300

    update locust scripts

--- scripts/locust/run-core.sh
+++ scripts/locust/run-core.sh
@@ -9,26 +9,31 @@ SPAWN=10
 CORES=8
 SERVER_HOST=http://127.0.0.1:8080
 RUN_TIME=40s
-NAME=default
+CSV_NAME=default
 
 if [ -n "$1" ]; then
-    NAME=$1
+    CSV_NAME=$1
 fi
 
+master() {
+  locust -f locustfile.py --headless -t $RUN_TIME --master-bind-port=$MASTER_PORT --master-bind-host=$MASTER_IP -u $USERS -r $SPAWN --only-summary --master --expect-workers=$CORES --csv $CSV_NAME --host=$SERVER_HOST&
+  echo "Master PID = $!"
+  sleep 5
+}
+
+slaves() {
+  pids=( )
+
+  for ((i = 1; i <= $CORES; i++)); do
+    locust -f locustfile.py --worker --master-host=$MASTER_IP --master-port=$MASTER_PORT &
+    pids+=( $! )
+  done
+
+  echo "Slave PIDs = ${pids[@]}"
+}
+
 echo -e "\nStart Master\n"
-locust -f locustfile.py --headless -t $RUN_TIME --master-bind-port=$MASTER_PORT --master-bind-host=$MASTER_IP -u $USERS -r $SPAWN --only-summary --master --expect-workers=$CORES --csv $NAME --host=$SERVER_HOST&
-
-PID_MASTER=$!
-echo "Master PID = $PID_MASTER"
-
-sleep 5
+master
 
 echo -e "\nStart Slaves\n"
-PID_SLAVES=( )
-
-for ((i = 1; i <= $CORES; i++));do
-  locust -f locustfile.py --worker --master-host=$MASTER_IP --master-port=$MASTER_PORT &
-  PID_SLAVES+=( $! )
-done
-
-echo "Slave PIDs = ${PID_SLAVES[@]}"
+slaves
