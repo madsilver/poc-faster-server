bf97ef7590b4faf3cac1dfbabb325f82a92b83e3
Rodrigo Prata <rodrigo-p-braga@telecom.com>
2022-06-02 07:20:18 -0300

    fix jooby ab response

--- poc-jooby/src/main/java/app/App.java
+++ poc-jooby/src/main/java/app/App.java
@@ -1,14 +1,14 @@
 package app;
 
-import app.controller.HealthcheckController;
+import io.jooby.AccessLogHandler;
 import io.jooby.Jooby;
 
 public class App extends Jooby {
     {
-        mvc(new HealthcheckController());
+        decorator(new AccessLogHandler());
+        get("/healthcheck", ctx -> "{\"message\":\"It works\"}");
     }
     public static void main(String[] args) {
         runApp(args, App::new);
     }
-
 }
