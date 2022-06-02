4ed4c37f9825d8b916f9f98582a433516c817606
Rodrigo Prata <rodrigo-p-braga@telecom.com>
2022-06-02 09:11:08 -0300

    set graceful shutdown

--- poc-jooby/src/main/java/app/App.java
+++ poc-jooby/src/main/java/app/App.java
@@ -1,11 +1,19 @@
 package app;
 
-import io.jooby.AccessLogHandler;
+import io.jooby.GracefulShutdown;
 import io.jooby.Jooby;
+import io.jooby.ServerOptions;
 
 public class App extends Jooby {
     {
-        decorator(new AccessLogHandler());
+        final int port = getEnvironment()
+                .getConfig()
+                .getInt("port");
+
+        setServerOptions(new ServerOptions().setPort(port));
+
+        install(new GracefulShutdown());
+
         get("/healthcheck", ctx -> "{\"message\":\"It works\"}");
     }
     public static void main(String[] args) {
